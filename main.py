a4db233a1fffb3569534dde5cee0cad4ab0ebfb5
Tom Chauvel <tom.chauvel@hotmail.com>
2023-09-04 21:39:50 +0200

    est-ce que ça va péter ?

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import datetime
 import json
+import re
 
-with open("./url.csv") as file:
-    salles = file.read().split("\n")
-    
+regex = re.compile( r"[A-z](.*) - (([A-z]?[-0-9]+)|([A-zÀ-ÿ ]*))" )
+
 def to_date(char):
     year = int(char[0:4])
     month = int(char[4:6])
@@ -15,50 +15,91 @@ def to_date(char):
     date = datetime.datetime(year, month, day, hour, minute, sec)
     return int(date.timestamp())
 
+def formatKey(key):
+    liste = key.split(" - ")
+
+    matches = regex.findall(key)[0]
+    
+    bat = matches[0]
+    code = "Salle"
+    salle = matches[1]
+
+    if salle.startswith("Hall "):
+        code = "Hall"
+        salle = salle.replace("Hall ","").lower()
+    elif salle.startswith("Amphi "):
+        code = "Amphi"
+        salle = salle.replace("Amphi ","").lower()
+
+    salle = "_".join(k for k in salle.split(" ") if k != "")
+
+    return "Bâtiment_" + bat + "_" + code + "_" + salle
+
+def batimentSupported(key) : 
+    liste = ["41","42","02B"]
+    return key.split("_")[1] in liste
+
 salles_liste = {}
 
-for item in salles :
-    if item == "" : break
+url = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/KYND2kWv.shu"
 
-    batiment,salle,url = item.split(",")
-    salle_code = batiment.replace(" ","_") + "_" + salle.replace(" ","_")
+req = requests.get(url)
+if req.status_code != 200 : raise Exception("Api Univ Pété") 
 
-    salles_liste[salle_code] = {"batiment":batiment,"salle":salle}
+liste = {}
 
-    req = requests.get(url)
-    if req.status_code != 200 : raise Exception("Api Univ Pété") 
+DTSTART=""
+DTEND=""
+SUMMARY=""
+LOCATIONS=""
 
-    liste = []
+text = req.text
+text = text.replace("\r","")
+text = text.replace("\n ","")
+text = text.split("\n")
 
-    DTSTART=""
-    DTEND=""
-    SUMMARY=""
-    
-    text = req.text
-    text = text.replace("\r","\n")
-    text = text.replace("\n ","")
-    text = text.split("\n")
 
-    for item in text:
-        line = item.split(":")
-        code = line[0]
-        value = ":".join(line[1:])
+for item in text:
+    line = item.split(":")
+    code = line[0]
+    value = ":".join(line[1:])
 
-        if code == "DTSTART":
-            DTSTART=to_date(value)
-        elif code == "DTEND":
-            DTEND=to_date(value)
-        elif code == "SUMMARY":
-            SUMMARY=value
-        elif code == "END" and value == "VEVENT":
-            liste.append([DTSTART , DTEND , SUMMARY])
-    print(liste[0],salle_code)
+    if code == "DTSTART":
+        DTSTART=to_date(value)
+    elif code == "DTEND":
+        DTEND=to_date(value)
+    elif code == "LOCATION":
+        LOCATIONS=value
+    elif code == "SUMMARY":
+        SUMMARY=value
 
-    liste.sort()
-    with open("./out/"+salle_code+".json","w+") as file:
-        print(liste[0],salle_code)
+    elif code == "END" and value == "VEVENT":
+        for loc in LOCATIONS.split("\,"):
 
-        file.write(json.dumps(liste))
+            LOCATION = formatKey(loc)
+
+            if ( not batimentSupported(LOCATION) ) :
+                continue
+
+
+            if LOCATION not in liste:
+                liste[LOCATION] = []
+            liste[LOCATION].append([DTSTART , DTEND , SUMMARY])
 
 with open("./out/salles.json","w+") as file:
-    file.write(json.dumps(salles_liste))
+    keyDic = {}
+    for key in liste.keys():
+        val = key.split("_")
+        keyDic[key] = {
+            "batiment":" ".join(val[0:2]),
+            "salle":" ".join(val[2:])
+        }
+    file.write(json.dumps(keyDic))
+
+for key in liste :
+    value = liste[key]
+
+    value.sort()
+    with open("./out/"+key+".json","w+") as file:
+
+        file.write(json.dumps(value))
